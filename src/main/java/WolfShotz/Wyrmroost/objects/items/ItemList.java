d35882c09764a953cab8ca41fe909000703be183
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-07-01 19:43:16 -0400

    Geode Armor sets + Plat. Prep.

--- src/main/java/WolfShotz/Wyrmroost/objects/items/ItemList.java
+++ src/main/java/WolfShotz/Wyrmroost/objects/items/ItemList.java
@@ -1,10 +1,13 @@
 package WolfShotz.Wyrmroost.objects.items;
 
 import WolfShotz.Wyrmroost.objects.items.base.*;
-import net.minecraft.item.IItemTier;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +19,30 @@ public class ItemList
 
     // Geode
     public static final Item itemgeode = new ItemBase("geode", ItemGroup.MATERIALS);
-    public static final Item itemswordgeode = new ItemSwordBase("geode_sword", 4, ToolMaterialList.geode);
-    public static final Item itempickgeode = new ItemPickBase("geode_pick", ToolMaterialList.geode);
-    public static final Item itemshovelgeode = new ItemShovelBase("geode_shovel", ToolMaterialList.geode);
-    public static final Item itemaxegeode = new ItemAxeBase("geode_axe", ToolMaterialList.geode);
+
+    public static final Item itemswordgeode = new ItemSwordBase("geode_sword", 4, ToolMaterialList.tool_geode);
+    public static final Item itempickgeode = new ItemPickBase("geode_pick", ToolMaterialList.tool_geode);
+    public static final Item itemshovelgeode = new ItemShovelBase("geode_shovel", ToolMaterialList.tool_geode);
+    public static final Item itemaxegeode = new ItemAxeBase("geode_axe", ToolMaterialList.tool_geode);
+
+    public static final Item itemgeodehelm = new ItemArmorBase("geode_helmet", ArmorMaterialList.armor_geode, EquipmentSlotType.HEAD);
+    public static final Item itemgeodechest = new ItemArmorBase("geode_chestplate", ArmorMaterialList.armor_geode, EquipmentSlotType.CHEST);
+    public static final Item itemgeodelegs = new ItemArmorBase("geode_legs", ArmorMaterialList.armor_geode, EquipmentSlotType.LEGS);
+    public static final Item itemgeodeboots = new ItemArmorBase("geode_boots", ArmorMaterialList.armor_geode, EquipmentSlotType.FEET);
+
+    // Platinum
+    public static final Item itemplatinumingot = new ItemBase("platinum_ingot", ItemGroup.MATERIALS);
+
+    public static final Item itemplatinumsword = new ItemSwordBase("platinum_sword", 3, ToolMaterialList.tool_platinum);
+    public static final Item itemplatinumpick = new ItemPickBase("platinum_pick", ToolMaterialList.tool_platinum);
+    public static final Item itemplatinumaxe = new ItemAxeBase("platinum_axe", ToolMaterialList.tool_platinum);
+    public static final Item itemplatinumshovel = new ItemShovelBase("platinum_shovel", ToolMaterialList.tool_platinum);
+
+    public static final Item itemplatinumhelm = new ItemArmorBase("platinum_helmet", ArmorMaterialList.armor_platinum, EquipmentSlotType.HEAD);
+    public static final Item itemplatinumchest = new ItemArmorBase("platinum_chestplate", ArmorMaterialList.armor_platinum, EquipmentSlotType.CHEST);
+    public static final Item itemplatinumlegs = new ItemArmorBase("platinum_leggins", ArmorMaterialList.armor_platinum, EquipmentSlotType.LEGS);
+    public static final Item itemplatinumboots = new ItemArmorBase("platinum_boots", ArmorMaterialList.armor_platinum, EquipmentSlotType.FEET);
+
 
 
 
@@ -28,7 +51,8 @@ public class ItemList
     /** Enum Handling the tool materials - Manages the mining speed, attck dmg, repair item etc. */
     public enum ToolMaterialList implements IItemTier
     {
-        geode(9.3f, 4.0f, 2164, 4, 20, itemgeode);
+        tool_geode(9.3f, 4.0f, 2164, 4, 25, itemgeode),
+        tool_platinum(5.5f, 2.5f, 645, 3, 20, itemplatinumingot);
 
         private float efficiency, attackDamage;
         private int durability, harvestLevel, enchantibility;
@@ -60,6 +84,52 @@ public class ItemList
 
         @Override
         public Ingredient getRepairMaterial() { return Ingredient.fromItems(repairMaterial); }
+    }
+
+    /** Enum Handling the armor materials - Manages dmg reduction, enchantability, durability etc. */
+    public enum ArmorMaterialList implements IArmorMaterial
+    {
+        armor_geode("geode", new int[] {4, 7, 9, 4}, 2.8f, 48, 25, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, itemgeode),
+        armor_platinum("platinum", new int[] {2, 5, 7, 2}, 0.2f, 20, 10, SoundEvents.ITEM_ARMOR_EQUIP_IRON, itemplatinumingot);
+
+        private int durability, enchantability;
+        private int[] dmgReduction;
+        private float toughness;
+        private String name;
+        private SoundEvent sound;
+        private Item repairMaterial;
+
+        ArmorMaterialList(String name, int[] dmgReduction, float toughness, int durability, int enchantability, SoundEvent sound, Item repairMaterial) {
+            this.durability = durability;
+            this.dmgReduction = dmgReduction;
+            this.enchantability = enchantability;
+            this.toughness = toughness;
+            this.name = name;
+            this.sound = sound;
+            this.repairMaterial = repairMaterial;
+        }
+
+        @Override
+        public int getDurability(EquipmentSlotType slotIn) { return durability; }
+
+        @Override
+        public int getDamageReductionAmount(EquipmentSlotType slotIn) { return dmgReduction[slotIn.getIndex()]; }
+
+        @Override
+        public int getEnchantability() { return enchantability; }
+
+        @Override
+        public SoundEvent getSoundEvent() { return sound; }
+
+        @Override
+        public Ingredient getRepairMaterial() { return Ingredient.fromItems(repairMaterial); }
+
+        @Override
+        @OnlyIn(Dist.CLIENT)
+        public String getName() { return name; }
+
+        @Override
+        public float getToughness() { return toughness; }
     }
 
 }
