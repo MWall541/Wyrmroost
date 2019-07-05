b8e3acd4c5a3375f5f21977682cc252ee26b8f9a
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-07-05 17:38:20 -0400

    Begin Work on... oop, no spoils :eyes:

---
+++ src/main/java/WolfShotz/Wyrmroost/content/world/biomes/BiomeFoggyPine.java
@@ -0,0 +1,11 @@
+package WolfShotz.Wyrmroost.content.world.biomes;
+
+import net.minecraft.world.biome.Biome;
+
+public class BiomeFoggyPine extends Biome
+{
+    public BiomeFoggyPine() {
+        super(new Biome.Builder().waterColor(0x006130).temperature(1.0f));
+
+    }
+}
