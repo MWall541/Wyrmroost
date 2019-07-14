45ad75643f2e8665ecd48ed827806cad51335acd
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-07-14 18:08:50 -0400

    +Volante-ah whatever the fuck it is

--- src/main/java/WolfShotz/Wyrmroost/util/ModUtils.java
+++ src/main/java/WolfShotz/Wyrmroost/util/ModUtils.java
@@ -10,6 +10,8 @@ import net.minecraft.util.text.TranslationTextComponent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Collection;
+
 /**
  * Created by WolfShotz 7/9/19 - 00:31
  * Utility Class to help with Internationalization, ResourceLocations, etc.
@@ -61,4 +63,12 @@ public class ModUtils
      * @param text
      */
     public static String clean(String text) { return text.replace(" ", ""); }
+
+    // ===========
+    // Collections
+    // ===========
+
+    /** Cleaner Way of adding elements to a collection */
+    public static void collectAll(Collection collection, Object... objects)
+        { for (Object object : objects) collection.add(object); }
 }
