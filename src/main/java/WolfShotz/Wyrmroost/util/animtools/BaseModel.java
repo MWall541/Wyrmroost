b76027f0de1f4c90ab32f38bbfa258ca58eb2874
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-07-14 18:08:20 -0400

    Animation Utils to make my life suck less
    Credits: @gegy1000

--- src/main/java/WolfShotz/Wyrmroost/util/animtools/BaseModel.java
+++ src/main/java/WolfShotz/Wyrmroost/util/animtools/BaseModel.java
@@ -2,10 +2,82 @@ package WolfShotz.Wyrmroost.util.animtools;
 
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
 
 public class BaseModel<T extends Entity> extends EntityModel<T>
 {
-    public void setDefaultPoses() { boxList.forEach(box -> ((BaseRenderer) box).storeBoxPose()); }
+    public void setDefaultPoses() {
+        boxList.stream()
+                .filter(modelRenderer -> modelRenderer instanceof BaseRenderer)
+                .forEach(box -> ((BaseRenderer) box).storeBoxPose());
+    }
 
-    public void restorePoses() { boxList.forEach(box -> ((BaseRenderer) box).restoreBoxPose()); }
+    public void restorePoses() {
+        boxList.stream()
+                .filter(modelRenderer -> modelRenderer instanceof BaseRenderer)
+                .forEach(box -> ((BaseRenderer) box).restoreBoxPose());
+    }
+
+    /**
+     * Rotates this box back and forth (rotateAngleX). Useful for arms and legs.
+     */
+    public void rotX(BaseRenderer box, float speed, float degree, boolean invert, float offset, float weight, float walk, float walkAmount)
+        { box.rotX(speed, degree, invert, offset, weight, walk, walkAmount); }
+
+    /**
+     * Rotates this box up and down (rotateAngleZ). Useful for wing and ears.
+     */
+    public void rotZ(BaseRenderer box, float speed, float degree, boolean invert, float offset, float weight, float flap, float flapAmount)
+        { box.rotZ(speed, degree, invert, offset, weight, flap, flapAmount); }
+
+    /**
+     * Rotates this box side to side (rotateAngleY).
+     */
+    public void rotY(BaseRenderer box, float speed, float degree, boolean invert, float offset, float weight, float swing, float swingAmount)
+        { box.rotY(speed, degree, invert, offset, weight, swing, swingAmount); }
+
+    /**
+     * Moves this box up and down (rotationPointY). Useful for bodies.
+     */
+    public void moveY(BaseRenderer box, float speed, float degree, boolean bounce, float f, float f1)
+        { box.moveY(speed, degree, bounce, f, f1); }
+
+    /**
+     * Rotates (on the Y axis) the given model parts in a chain-like manner.
+     */
+    public void chainRotY(BaseRenderer[] boxes, float speed, float degree, double rootOffset, float swing, float swingAmount) {
+        float offset = this.calculateChainOffset(rootOffset, boxes);
+        for (int index = 0; index < boxes.length; index++) {
+            boxes[index].rotateAngleY += calculateChainRotation(speed, degree, swing, swingAmount, offset, index);
+        }
+    }
+
+    /**
+     * Rotates (on the X axis) the given model parts in a chain-like manner.
+     */
+    public void chainRotX(BaseRenderer[] boxes, float speed, float degree, double rootOffset, float swing, float swingAmount) {
+        float offset = this.calculateChainOffset(rootOffset, boxes);
+        for (int index = 0; index < boxes.length; index++) {
+            boxes[index].rotateAngleX += calculateChainRotation(speed, degree, swing, swingAmount, offset, index);
+        }
+    }
+
+    /**
+     * Rotates (on the Z axis) the given model parts in a chain-like manner.
+     */
+    public void chainRotZ(BaseRenderer[] boxes, float speed, float degree, double rootOffset, float swing, float swingAmount) {
+        float offset = this.calculateChainOffset(rootOffset, boxes);
+        for (int index = 0; index < boxes.length; index++) {
+            boxes[index].rotateAngleZ += calculateChainRotation(speed, degree, swing, swingAmount, offset, index);
+        }
+    }
+
+    private float calculateChainRotation(float speed, float degree, float swing, float swingAmount, float offset, int boxIndex) {
+        return MathHelper.cos(swing * (speed * 1) + offset * boxIndex) * swingAmount * (degree * 1);
+    }
+
+    private float calculateChainOffset(double rootOffset, BaseRenderer... boxes) {
+        return (float) ((rootOffset * Math.PI) / (2 * boxes.length));
+    }
+
 }
--- src/main/java/WolfShotz/Wyrmroost/util/animtools/BaseRenderer.java
+++ src/main/java/WolfShotz/Wyrmroost/util/animtools/BaseRenderer.java
@@ -1,6 +1,7 @@
 package WolfShotz.Wyrmroost.util.animtools;
 
 import net.minecraft.client.renderer.entity.model.RendererModel;
+import net.minecraft.util.math.MathHelper;
 
 public class BaseRenderer extends RendererModel
 {
@@ -8,8 +9,13 @@ public class BaseRenderer extends RendererModel
     public float defaultRotX, defaultRotY, defaultRotZ;
     public float defaultOffX, defaultOffY, defaultOffZ;
 
-    public BaseRenderer(BaseModel model, String boxname) {
-        super(model, boxname);
+    public BaseRenderer(BaseModel model, String boxname) { super(model, boxname); }
+
+    public BaseRenderer(BaseModel model) { this(model, (String)null); }
+
+    public BaseRenderer(BaseModel model, int texOffX, int texOffY) {
+        this(model);
+        this.setTextureOffset(texOffX, texOffY);
     }
 
     /** Store the position of a box. */
@@ -40,5 +46,32 @@ public class BaseRenderer extends RendererModel
         offsetX = defaultOffX;
         offsetY = defaultOffY;
         offsetZ = defaultOffZ;
+    }
+
+    /** Rotates this box back and forth (rotateAngleX). */
+    public void rotX(float speed, float degree, boolean invert, float offset, float weight, float walk, float walkAmount) {
+        this.rotateAngleX += this.calculateRotation(speed, degree, invert, offset, weight, walk, walkAmount);
+    }
+
+    /** Rotates this box up and down (rotateAngleZ). */
+    public void rotZ(float speed, float degree, boolean invert, float offset, float weight, float flap, float flapAmount) {
+        this.rotateAngleZ += this.calculateRotation(speed, degree, invert, offset, weight, flap, flapAmount);
+    }
+
+    /** Rotates this box side to side (rotateAngleY). */
+    public void rotY(float speed, float degree, boolean invert, float offset, float weight, float swing, float swingAmount) {
+        this.rotateAngleY += this.calculateRotation(speed, degree, invert, offset, weight, swing, swingAmount);
+    }
+
+    /** Moves this box up and down (rotationPointY). */
+    public void moveY(float speed, float degree, boolean bounce, float f, float f1) {
+        float bob = (float) (Math.sin(f * speed) * f1 * degree - f1 * degree);
+        if (bounce) bob = (float) -Math.abs((Math.sin(f * speed) * f1 * degree));
+        this.rotationPointY += bob;
+    }
+
+    private float calculateRotation(float speed, float degree, boolean invert, float offset, float weight, float f, float f1) {
+        float rotation = (MathHelper.cos(f * (speed) + offset) * (degree) * f1) + (weight * f1);
+        return invert ? -rotation : rotation;
     }
 }
