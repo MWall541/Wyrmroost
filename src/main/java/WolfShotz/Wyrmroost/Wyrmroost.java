c10870b40e45d9778bc823a58649c59cede2dee3
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-08-09 03:51:59 -0400

    Simple Network Channel is setup!

--- src/main/java/WolfShotz/Wyrmroost/Wyrmroost.java
+++ src/main/java/WolfShotz/Wyrmroost/Wyrmroost.java
@@ -2,25 +2,37 @@ package WolfShotz.Wyrmroost;
 
 import WolfShotz.Wyrmroost.content.blocks.eggblock.EggRenderer;
 import WolfShotz.Wyrmroost.content.blocks.eggblock.EggTileEntity;
-import WolfShotz.Wyrmroost.event.ForgeEvents;
-import WolfShotz.Wyrmroost.event.SetupEntities;
-import WolfShotz.Wyrmroost.event.SetupItems;
-import WolfShotz.Wyrmroost.event.SetupOreGen;
+import WolfShotz.Wyrmroost.event.*;
+import WolfShotz.Wyrmroost.network.AnimationMessage;
+import WolfShotz.Wyrmroost.network.SendKeyPressMessage;
 import WolfShotz.Wyrmroost.util.ModUtils;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 
 @Mod(Wyrmroost.modID)
 public class Wyrmroost
 {
     public static final String modID = "wyrmroost";
     public static final ItemGroup creativeTab = new CreativeTab();
+    
+    public static final String channel = modID;
+    private static final String channelver = "1.0";
+    public static SimpleChannel network = NetworkRegistry.ChannelBuilder
+            .named(ModUtils.location(channel))
+            .clientAcceptedVersions(channelver::equals)
+            .serverAcceptedVersions(channelver::equals)
+            .networkProtocolVersion(() -> channelver)
+            .simpleChannel();
 
     public Wyrmroost() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
@@ -31,19 +43,33 @@ public class Wyrmroost
         MinecraftForge.EVENT_BUS.addListener(ForgeEvents::cancelFall);
         
         SetupOreGen.setupOreGen();
+        
+        int networkIndex = 0;
+        network.registerMessage(++networkIndex, AnimationMessage.class, AnimationMessage::encode, AnimationMessage::new, AnimationMessage::handle);
+        network.registerMessage(++networkIndex, SendKeyPressMessage.class, SendKeyPressMessage::encode, SendKeyPressMessage::new, SendKeyPressMessage::handle);
 
-        ModUtils.L.debug("commonSetup complete");
+        ModUtils.L.debug("Fired FMLCommon Setup");
     }
 
     private void clientSetup(final FMLClientSetupEvent event) {
 //        MinecraftForge.EVENT_BUS.addListener(ForgeEvents::ridingPerspective);
+        MinecraftForge.EVENT_BUS.addListener(ForgeEvents::onKeyPress);
         
         SetupEntities.registerEntityRenders();
-        ClientRegistry.bindTileEntitySpecialRenderer(EggTileEntity.class, new EggRenderer());
+        SetupKeyBinds.registerKeys();
+        registerSpecialRenders();
 
-        ModUtils.L.info("clientSetup complete");
+        ModUtils.L.info("Fired FMLClientSetup");
     }
-
+    
+    /**
+     * Needs to be in seperate <code>@OnlyIn</code> annotated method otherwise servers throw a fit...
+     */
+    @OnlyIn(Dist.CLIENT)
+    private void registerSpecialRenders() {
+        ClientRegistry.bindTileEntitySpecialRenderer(EggTileEntity.class, new EggRenderer());
+    }
+    
     private static class CreativeTab extends ItemGroup
     {
         private CreativeTab() { super("wyrmroost"); }
---
+++ src/main/java/WolfShotz/Wyrmroost/util/NetworkUtils.java
@@ -0,0 +1,20 @@
+package WolfShotz.Wyrmroost.util;
+
+import WolfShotz.Wyrmroost.Wyrmroost;
+import WolfShotz.Wyrmroost.content.entities.AbstractDragonEntity;
+import WolfShotz.Wyrmroost.network.AnimationMessage;
+import com.github.alexthe666.citadel.animation.Animation;
+import net.minecraftforge.fml.network.PacketDistributor;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class NetworkUtils
+{
+    public static void sendAnimationPacket(AbstractDragonEntity dragon, Animation animation) {
+        if (dragon.world.isRemote) return;
+        
+        AnimationMessage message = new AnimationMessage(dragon, ArrayUtils.indexOf(dragon.getAnimations(), animation));
+    
+        dragon.setAnimation(animation);
+        Wyrmroost.network.send(PacketDistributor.TRACKING_ENTITY.with(() -> dragon), message);
+    }
+}
