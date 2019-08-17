9308c16230bf45f4940c2850ee7a687dae6fc11b
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-08-17 14:54:25 -0400

    dont code at 2am. never. (I did dumb things)

--- src/main/java/WolfShotz/Wyrmroost/content/entities/owdrake/OWDrakeRenderer.java
+++ src/main/java/WolfShotz/Wyrmroost/content/entities/owdrake/OWDrakeRenderer.java
@@ -16,17 +16,14 @@ public class OWDrakeRenderer extends AbstractDragonRenderer<OWDrakeEntity>
 {
     private final String loc = DEF_LOC + "owdrake/";
     private final ResourceLocation maleCom = ModUtils.location(loc + "male_com.png");
-    private final ResourceLocation femaleCom = ModUtils.location(loc + "female_com");
-    private final ResourceLocation maleComCld = ModUtils.location(loc + "male_com_cld");
-    private final ResourceLocation femaleComCld = ModUtils.location(loc + "female_com_cld");
+    private final ResourceLocation femaleCom = ModUtils.location(loc + "female_com.png");
     private final ResourceLocation maleSav = ModUtils.location(loc + "male_sav.png");
     private final ResourceLocation femaleSav = ModUtils.location(loc + "female_sav.png");
-    private final ResourceLocation maleSavCld = ModUtils.location(loc + "male_sav_cld");
-    private final ResourceLocation femaleSavCld = ModUtils.location(loc + "female_sav_cld");
     private final ResourceLocation maleAlb = ModUtils.location(loc + "male_alb.png");
-    private final ResourceLocation femaleAlb = ModUtils.location(loc + "female_alb");
-    private final ResourceLocation maleAlbCld = ModUtils.location(loc + "male_alb_cld");
-    private final ResourceLocation femaleAlbCld = ModUtils.location(loc + "female_alb_cld");
+    private final ResourceLocation femaleAlb = ModUtils.location(loc + "female_alb.png");
+    private final ResourceLocation childCom = ModUtils.location(loc + "child_com.png");
+    private final ResourceLocation childSav = ModUtils.location(loc + "child_sav.png");
+    private final ResourceLocation childAlb = ModUtils.location(loc + "child_alb.png");
     // Saddle
     private final ResourceLocation saddle = ModUtils.location(loc + "saddle.png");
     // Easter Egg
@@ -45,15 +42,13 @@ public class OWDrakeRenderer extends AbstractDragonRenderer<OWDrakeEntity>
     }
 
     private ResourceLocation getDrakeTexture(boolean gender, boolean isSavannah, boolean isAlbino, boolean isChild) {
-        if (gender) {
-            if (isAlbino) return isChild? maleAlbCld : maleAlb;
-            if (isSavannah) return isChild? maleSavCld : maleSav;
-        } else {
-            if (isAlbino) return isChild? femaleAlbCld : femaleAlb;
-            if (isSavannah) return isChild? femaleSavCld : femaleSav;
+        if (isChild) {
+            if (isSavannah) return childSav;
+            if (isAlbino) return childAlb;
+            return childCom;
         }
-        
-        return maleCom; // FALLBACK
+        if (isSavannah) return gender? maleSav : femaleSav;
+        if (isAlbino) return gender? maleAlb : femaleAlb;
+        return gender? maleCom : femaleCom;
     }
-
 }
