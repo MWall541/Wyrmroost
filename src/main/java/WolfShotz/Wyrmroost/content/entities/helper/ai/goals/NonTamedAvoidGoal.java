8d0d35443ac87d531fe3ab3c06343b3a716a6d6e
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-08-26 23:17:03 -0400

    Avoiding players via flight no longer sky rockets >.>

--- src/main/java/WolfShotz/Wyrmroost/content/entities/helper/ai/goals/NonTamedAvoidGoal.java
+++ src/main/java/WolfShotz/Wyrmroost/content/entities/helper/ai/goals/NonTamedAvoidGoal.java
@@ -9,8 +9,6 @@ public class NonTamedAvoidGoal extends AvoidEntityGoal
 {
     private AbstractDragonEntity dragon;
     private boolean flysAway;
-    private float speedFar;
-    private float speedNear;
     
     public NonTamedAvoidGoal(AbstractDragonEntity dragon, float distance, float speedNear, float speedFar) {
         this(dragon, PlayerEntity.class, distance, speedNear, speedFar, false);
@@ -20,18 +18,11 @@ public class NonTamedAvoidGoal extends AvoidEntityGoal
         super(dragon, avoidingClass, distance, speedNear, speedFar, EntityPredicates.CAN_AI_TARGET);
         this.dragon = dragon;
         this.flysAway = flysAway;
-        this.speedFar = speedFar;
-        this.speedNear = speedNear;
     }
     
-    /**
-     * Keep ticking a continuous task that has already been started
-     */
-    public void tick() {
-        if (entity.getDistanceSq(field_75376_d) < 49.0D) {
-            if (flysAway) dragon.setFlying(true);
-            else entity.getNavigator().setSpeed(speedFar);
-        } else entity.getNavigator().setSpeed(speedNear);
+    @Override
+    public void startExecuting() {
+        super.startExecuting();
+        if (flysAway) dragon.setFlying(true);
     }
-    
 }
