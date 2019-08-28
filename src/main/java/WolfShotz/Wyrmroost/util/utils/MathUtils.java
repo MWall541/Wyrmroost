a64f0f8fc9657b4c02da8919cd84f7bde654016d
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-08-28 19:22:29 -0400

    IM BAD AT MAAAAAATTTH

--- src/main/java/WolfShotz/Wyrmroost/util/utils/MathUtils.java
+++ src/main/java/WolfShotz/Wyrmroost/util/utils/MathUtils.java
@@ -27,22 +27,45 @@ public class MathUtils
     
     /**
      * Get the Altitude of an entity from the world surface
-     * Subtract 1 from posZ as a workaround for a vanilla bug using the wrong pos when posZ is negative...
+     * Subtract 1 from plane pose's as a workaround for a vanilla bug using the wrong pos when plane pose's are negative...
      */
-    public static double getAltitude(Entity entity) { return entity.posY - entity.world.getHeight(Heightmap.Type.MOTION_BLOCKING, (int) entity.posX, (int) entity.posZ - (entity.posZ < 0? 1 : 0)); }
+    public static double getAltitude(Entity entity) {
+        return entity.posY - entity.world.getHeight(Heightmap.Type.MOTION_BLOCKING, (int) entity.posX - (entity.posX < 0? 1 : 0), (int) entity.posZ - (entity.posZ < 0? 1 : 0));
+    }
     
     /**
      * Get the altitude of a world position from the world surface
-     * Subtract 1 from posZ as a workaround for a vanilla bug using the wrong pos when posZ is negative...
+     * Subtract 1 from plane pose's as a workaround for a vanilla bug using the wrong pos when plane pose's are negative...
      */
-    public static double getAltitude(World world, int x, double y, int z) { return y - world.getHeight(Heightmap.Type.WORLD_SURFACE, x, z - (z < 0? 1 : 0)); }
+    public static double getAltitude(World world, int x, double y, int z) {
+        return y - world.getHeight(Heightmap.Type.WORLD_SURFACE, x - (x < 0? 1 : 0), z - (z < 0? 1 : 0));
+    }
     
-    public static double calcDistance(double z, double z2, double x, double x2) {
+    /**
+     * Calculate the euclidean plane distance of two points.
+     * Double
+     */
+    public static double calcDistance2d(double z, double z2, double x, double x2) {
         return Math.sqrt((z - z2) * (z - z2) + (x - x2) * (x - x2));
     }
     
-    public static float calcDistance(float z, float z2, float x, float x2) {
+    /**
+     * Calculate the euclidean plane distance between two points.
+     * Float
+     */
+    public static float calcDistance2d(float z, float z2, float x, float x2) {
         return (float) Math.sqrt((z - z2) * (z - z2) + (x - x2) * (x - x2));
-    
     }
+    
+    /**
+     * Calculate euclidean space distance
+     * Double
+     */
+    public static double calcDistance3d(double x, double y, double z) { return x * x + y * y + z * z; }
+    
+    /**
+     * Calculate euclidean space distance
+     * Float
+     */
+    public static float calcDistance3d(float x, float y, float z) { return x * x + y * y + z * z; }
 }
