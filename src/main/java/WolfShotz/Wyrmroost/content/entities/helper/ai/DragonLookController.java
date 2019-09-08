db6905361dc459d990ce811d86e8687a37b66dc7
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-09-08 05:23:30 -0400

    Imagine having ur fucking neck turn 720 degrees

---
+++ src/main/java/WolfShotz/Wyrmroost/content/entities/helper/ai/DragonLookController.java
@@ -0,0 +1,22 @@
+package WolfShotz.Wyrmroost.content.entities.helper.ai;
+
+import WolfShotz.Wyrmroost.content.entities.dragon.AbstractDragonEntity;
+import net.minecraft.entity.ai.controller.LookController;
+
+//TODO
+public class DragonLookController extends LookController
+{
+    /** Casting, we <i>know</i> our mob is apart of our dragon base */
+    private AbstractDragonEntity dragon;
+    
+    public DragonLookController(AbstractDragonEntity dragon) {
+        super(dragon);
+        this.dragon = dragon;
+    }
+    
+    @Override
+    public void tick() {
+        if (!dragon.isSleeping())
+            super.tick();
+    }
+}
