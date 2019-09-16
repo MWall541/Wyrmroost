b75cef497d4d66fae5b6f542333676a9c780d8be
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-09-15 22:32:15 -0400

    Dragons are no longer owls

--- src/main/java/WolfShotz/Wyrmroost/util/entityhelpers/DragonBodyController.java
+++ src/main/java/WolfShotz/Wyrmroost/util/entityhelpers/DragonBodyController.java
@@ -2,6 +2,7 @@ package WolfShotz.Wyrmroost.util.entityhelpers;
 
 import WolfShotz.Wyrmroost.content.entities.dragon.AbstractDragonEntity;
 import net.minecraft.entity.ai.controller.BodyController;
+import net.minecraft.util.math.MathHelper;
 
 /**
  * Created by WolfShotz - 8/26/19 - 16:12
@@ -20,7 +21,15 @@ public class DragonBodyController extends BodyController
     @Override
     public void updateRenderAngles() {
         // No rotations while sitting or sleeping
-        if (dragon.isSitting() || dragon.isSleeping()) return;
+        if (dragon.isSleeping()) return;
+    
+        if (dragon.isSitting()) {
+            dragon.renderYawOffset = dragon.rotationYaw;
+            dragon.rotationYawHead = MathHelper.func_219800_b(dragon.rotationYawHead, dragon.renderYawOffset, (float) dragon.getHorizontalFaceSpeed());
+            
+            return;
+        }
+        
         super.updateRenderAngles();
     }
 }
