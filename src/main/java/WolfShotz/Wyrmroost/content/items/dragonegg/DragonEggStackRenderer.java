0f94d5718c4724ba2b00adcf4b647d3405e45529
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-12-10 00:43:26 -0500

    Modelbakery? just a fancy term for methlab

--- src/main/java/WolfShotz/Wyrmroost/content/items/dragonegg/DragonEggStackRenderer.java
+++ src/main/java/WolfShotz/Wyrmroost/content/items/dragonegg/DragonEggStackRenderer.java
@@ -20,9 +20,9 @@ public class DragonEggStackRenderer extends ItemStackTileEntityRenderer
         if (PROPERTIES == null) PROPERTIES = getEggProperties(itemStackIn);
         
         GlStateManager.pushMatrix();
-        GlStateManager.scalef(-1f, 1f, 1f);
-        GlStateManager.scalef(PROPERTIES.getSize().width * 2.95f, -(PROPERTIES.getSize().height * 2), -(PROPERTIES.getSize().width * 2.95f));
+        GlStateManager.scalef(1f, 1f, 1f);
         GlStateManager.color4f(1.5f, 1.5f, 1.5f, 1f);
+        
         Minecraft.getInstance().getTextureManager().bindTexture(PROPERTIES.getEggTexture());
         EGG_MODEL.base.render(0.0625f);
         GlStateManager.popMatrix();
