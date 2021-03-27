b1c1872280cdfffc031121a523161097f96c8773
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2021-03-26 22:19:06 -0400

    full databased features (configurable ores)

--- src/main/java/com/github/wolfshotz/wyrmroost/data/DataGatherer.java
+++ src/main/java/com/github/wolfshotz/wyrmroost/data/DataGatherer.java
@@ -1,7 +1,10 @@
 package com.github.wolfshotz.wyrmroost.data;
 
+import com.github.wolfshotz.wyrmroost.Wyrmroost;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
 /**
@@ -11,9 +14,14 @@ import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
  * data related shit in its own space. Thus the DataGatherer
  * This is because, when it comes to runtime, the object instances that hold that data will never even be used.
  * Nitpicky, but I don't care its saving someone that little bit of memory to squeeze that one last chrome tab in.
+ *
+ * Also, this class' package and everything in it IS NOT compiled with the jar, as it is not needed for production.
+ * We're just generating jsons, no need to have that delivered.
  */
+@Mod.EventBusSubscriber(modid = Wyrmroost.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGatherer
 {
+    @SubscribeEvent
     public static void gather(GatherDataEvent event)
     {
         DataGenerator gen = event.getGenerator();
@@ -24,6 +32,7 @@ public class DataGatherer
             TagData.provide(gen, fileHelper);
             gen.addProvider(new RecipeData(gen));
             gen.addProvider(new LootTableData(gen));
+            WorldData.provide(gen);
         }
         if (event.includeClient())
         {
---
+++ src/main/java/com/github/wolfshotz/wyrmroost/data/WorldData.java
@@ -0,0 +1,124 @@
+package com.github.wolfshotz.wyrmroost.data;
+
+import com.github.wolfshotz.wyrmroost.Wyrmroost;
+import com.github.wolfshotz.wyrmroost.registry.WRBlocks;
+import com.github.wolfshotz.wyrmroost.registry.WRWorld;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DirectoryCache;
+import net.minecraft.data.IDataProvider;
+import net.minecraft.world.gen.blockplacer.SimpleBlockPlacer;
+import net.minecraft.world.gen.blockstateprovider.ForestFlowerBlockStateProvider;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.placement.ChanceConfig;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.surfacebuilders.ConfiguredSurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class WorldData<T> implements IDataProvider
+{
+    private static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().create();
+    private static DataGenerator generator;
+
+    private final Map<String, JsonElement> built = new HashMap<>();
+    private final String name;
+    private final String path;
+    private final Codec<T> codec;
+
+    public WorldData(String name, String path, Codec<T> codec)
+    {
+        this.name = name;
+        this.path = path;
+        this.codec = codec;
+    }
+
+    protected abstract void begin();
+
+    protected void add(String name, T obj)
+    {
+        built.put(name, codec.encodeStart(JsonOps.INSTANCE, obj).getOrThrow(false, Wyrmroost.LOG::error));
+    }
+
+    @Override
+    public void run(DirectoryCache cache)
+    {
+        begin();
+        built.forEach((name, json) ->
+        {
+            try
+            {
+                IDataProvider.save(GSON, cache, json, generator.getOutputFolder().resolve("data/wyrmroost/worldgen/" + path + "/" + name + ".json"));
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+        });
+
+    }
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
+    static void provide(DataGenerator gen)
+    {
+        generator = gen;
+
+        gen.addProvider(new ConfiguredFeatures());
+        gen.addProvider(new SurfaceBuilders());
+    }
+
+    private static class ConfiguredFeatures extends WorldData<ConfiguredFeature<?, ?>>
+    {
+        ConfiguredFeatures()
+        {
+            super("World Features", "configured_feature", ConfiguredFeature.DIRECT_CODEC);
+        }
+
+        @Override
+        protected void begin()
+        {
+            add("ore_red_geode", Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, WRBlocks.RED_GEODE_ORE.get().defaultBlockState(), 4)).range(128).squared().count(8));
+            add("ore_purple_geode", WRWorld.Features.NO_EXPOSE_REPLACE.get().configured(new ReplaceBlockConfig(Blocks.END_STONE.defaultBlockState(), WRBlocks.PURPLE_GEODE_ORE.get().defaultBlockState())).range(80).squared().count(45));
+            add("ore_blue_geode", Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, WRBlocks.BLUE_GEODE_ORE.get().defaultBlockState(), 10)).range(16).squared());
+            add("ore_platinum", Feature.ORE.configured(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, WRBlocks.PLATINUM_ORE.get().defaultBlockState(), 9)).range(64).squared().count(20));
+            add("better_water_lake", WRWorld.Features.BETTER_LAKE.get().configured(new BlockStateFeatureConfig(Blocks.WATER.defaultBlockState())).decorated(Placement.WATER_LAKE.configured(new ChanceConfig(4))));
+            add("patch_gilla_bush", Feature.RANDOM_PATCH.configured(new BlockClusterFeatureConfig.Builder(new SimpleBlockStateProvider(WRBlocks.GILLA.get().defaultBlockState()), SimpleBlockPlacer.INSTANCE).noProjection().build()).decorated(Features.Placements.ADD_32).decorated(Features.Placements.HEIGHTMAP_SQUARE).count(7));
+            add("tincture_flowers", Feature.FLOWER.configured(new BlockClusterFeatureConfig.Builder(ForestFlowerBlockStateProvider.INSTANCE, SimpleBlockPlacer.INSTANCE).build()).decorated(Features.Placements.ADD_32).decorated(Features.Placements.HEIGHTMAP_SQUARE).count(10));
+        }
+    }
+
+    private static class SurfaceBuilders extends WorldData<ConfiguredSurfaceBuilder<?>>
+    {
+        SurfaceBuilders()
+        {
+            super("Surface Builders", "configured_surface_builder", ConfiguredSurfaceBuilder.DIRECT_CODEC);
+        }
+
+        @Override
+        protected void begin()
+        {
+            add("mulch", SurfaceBuilder.DEFAULT.configured(config(WRBlocks.MULCH.get(), Blocks.DIRT, Blocks.GRAVEL)));
+        }
+
+        static SurfaceBuilderConfig config(Block topSoil, Block underSoil, Block underWaterMaterial)
+        {
+            return new SurfaceBuilderConfig(topSoil.defaultBlockState(), underSoil.defaultBlockState(), underWaterMaterial.defaultBlockState());
+        }
+    }
+}
